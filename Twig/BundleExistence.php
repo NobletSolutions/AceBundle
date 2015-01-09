c8478cf55332ca4b76da5aa5dc1875bbd312ac1d
Nathanael d. Noblet <nathanael@noblet.ca>
2015-01-09 14:24:35 -0700

    conditinal inclusion of punkave file uploader

---
+++ Twig/BundleExistence.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace NS\AceBundle\Twig;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Description of BundleExistence
+ *
+ * @author gnat
+ */
+class BundleExistence extends \Twig_Extension
+{
+    protected $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function getFunctions()
+    {
+        return array(
+            new \Twig_SimpleFunction('bundleExists', array($this, 'bundleExists')),
+        );
+    }
+
+    public function bundleExists($bundle)
+    {
+        return array_key_exists($bundle, $this->container->getParameter('kernel.bundles'));
+    }
+
+    public function getName()
+    {
+        return 'ms_ace_bundle_existence';
+    }
+}
