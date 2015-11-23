b263fb534323d9f6033c6c3d4896cfc9a6c4f55c
Mark Woynarski <mark.woynarski@nobletdesign.com>
2015-11-23 13:53:14 -0600

    Tweaks to datepicker

--- Resources/public/js/nsbootstrap.js
+++ Resources/public/js/nsbootstrap.js
@@ -66,7 +66,7 @@ $(document).click(function(ev)
     }
 });
 
-$(document).on('nsFormUpdate shown.bs.tab', function(ev)
+$(document).on('nsFormUpdate shown.bs.tab sonata.add_element', function(ev)
 {
     $('.date-picker').each(function(i, el)
     {
