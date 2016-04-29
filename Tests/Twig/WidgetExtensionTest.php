7293c9f7aae295847e2fdf6cf411bed9044cb639
Nathanael d. Noblet <nathanael@noblet.ca>
2016-04-29 13:23:05 -0600

    added options to the widget extension

---
+++ Tests/Twig/WidgetExtensionTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace NS\AceBundle\Tests\Twig;
+use NS\AceBundle\Twig\WidgetExtension;
+
+/**
+ * Created by PhpStorm.
+ * User: gnat
+ * Date: 29/04/16
+ * Time: 1:01 PM
+ */
+class WidgetExtensionTest extends \PHPUnit_Framework_TestCase
+{
+//    private $options = array(
+//        'filter_id' => 'filters',
+//        'open_icon' => 'fa-plus',
+//        'close_icon' => 'fa-minus',
+//        'target_text' => 'Filters',
+//        'include_expander' => true,
+//    );
+
+    /**
+     * @param $options
+     * @param $contains
+     *
+     * @dataProvider getOptions
+     */
+    public function testGetFilterString($options,$contains)
+    {
+        $extension = new WidgetExtension();
+        $output = $extension->getFilterString($options);
+
+        foreach($contains as $contain) {
+            $this->assertContains($contain, $output);
+        }
+    }
+
+    public function getOptions()
+    {
+        $defaults = array(
+            'filter_id' => 'filters',
+            'open_icon' => 'fa-plus',
+            'close_icon' => 'fa-minus',
+            'target_text' => 'Filters',
+        );
+
+        $second = $defaults;
+        $second['open_icon'] = 'fa-cog';
+
+        return array(
+            array($defaults, $defaults),
+            array($second,$second),
+        );
+    }
+
+    public function testGetFullScreen()
+    {
+        $extension = new WidgetExtension();
+        $this->assertNull($extension->getFullScreenString(array('include_expander'=>false)));
+        $this->assertNotEmpty($extension->getFullScreenString(array('include_expander'=>true)));
+    }
+}
--- Twig/WidgetExtension.php
+++ Twig/WidgetExtension.php
@@ -12,6 +12,17 @@ namespace NS\AceBundle\Twig;
 class WidgetExtension extends \Twig_Extension
 {
     /**
+     * @var array
+     */
+    private $options = array(
+        'filter_id' => 'filters',
+        'open_icon' => 'fa-plus',
+        'close_icon' => 'fa-minus',
+        'target_text' => 'Filters',
+        'include_expander' => true,
+    );
+
+    /**
      * @inheritDoc
      */
     public function getFunctions()
@@ -21,19 +32,44 @@ class WidgetExtension extends \Twig_Extension
         );
     }
 
-    public function renderFilterToolbox()
+    /**
+     * @param array $options
+     * @return string
+     */
+    public function renderFilterToolbox(array $options = array())
     {
-        return '<div class="widget-toolbar">
-                    <a href="#filters" data-toggle="collapse" class="white small">
-                        <i class="ace-icon fa fa-plus" data-icon-hide="fa-minus" data-icon-show="fa-plus"></i>
-                        Filters
-                    </a>
-                </div>
-                <div class="widget-toolbar no-border">
+        $applied = array_merge($this->options,$options);
+
+        return sprintf('%s%s',$this->getFilterString($applied),$this->getFullScreenString($applied));
+    }
+
+    /**
+     * @param array $options
+     * @return mixed
+     */
+    public function getFilterString(array $options = array())
+    {
+        return sprintf('<div class="widget-toolbar">
+                            <a href="#%s" data-toggle="collapse" class="white small"><i class="ace-icon fa %s" data-icon-hide="%s" data-icon-show="%s"></i> %s</a>
+                        </div>',$options['filter_id'],$options['open_icon'],$options['close_icon'],$options['open_icon'],$options['target_text']);
+    }
+
+    /**
+     * @param array $options
+     * @return null|string
+     */
+    public function getFullScreenString(array $options = array())
+    {
+        $output = null;
+        if($options['include_expander'] === true) {
+            $output = '<div class="widget-toolbar no-border">
                     <a href="#" data-action="fullscreen" class="white">
                         <i class="ace-icon fa fa-expand"></i>
                     </a>
                 </div>';
+        }
+
+        return $output;
     }
 
     /**
