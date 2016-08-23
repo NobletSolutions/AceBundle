343e2b753b0fd70acd0d053192e496f5c676c190
Nathanael d. Noblet <nathanael@noblet.ca>
2016-08-23 14:45:06 -0600

    added hidden-parent/child form extension

---
+++ Form/Extensions/HiddenParentChildExtension.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gnat
+ * Date: 23/08/16
+ * Time: 2:35 PM
+ */
+
+namespace NS\AceBundle\Form\Extensions;
+
+
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class HiddenParentChildExtension extends AbstractTypeExtension
+{
+    /**
+     * @inheritDoc
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        if (isset($options['hidden-parent'])) {
+            $view->vars['attr']['data-context-parent'] = $options['hidden-parent'];
+        }
+
+        if (isset($options['hidden-child'])) {
+            $view->vars['attr']['data-context-child'] = $options['hidden-child'];
+        }
+
+        if (isset($options['hidden-value'])) {
+            $view->vars['attr']['data-context-value'] = $options['hidden-value'];
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefined(['hidden-parent', 'hidden-child', 'hidden-value']);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getExtendedType()
+    {
+        return FormType::class;
+    }
+
+}
