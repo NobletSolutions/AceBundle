fe58f0fd6104083c48af34de9635c01705f1ff43
Nathanael d. Noblet <nathanael@noblet.ca>
2016-12-20 17:14:16 -0700

    added form theme configuration

--- NSAceBundle.php
+++ NSAceBundle.php
@@ -2,11 +2,18 @@
 
 namespace NS\AceBundle;
 
+use NS\AceBundle\DependencyInjection\Compiler\TwigFormThemeCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-/**
- * 
- */
 class NSAceBundle extends Bundle
 {
+    /**
+     * @inheritDoc
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $container->addCompilerPass(new TwigFormThemeCompilerPass());
+    }
 }
