72b22e30ea234c6740e8795291bf18e9518c0463
Nathanael d. Noblet <nathanael@noblet.ca>
2017-04-24 13:58:48 -0600

    added readmore expander feature

--- src/Resources/public/js/nsbootstrap.js
+++ src/Resources/public/js/nsbootstrap.js
@@ -47,6 +47,25 @@ $(document).ready(function() {
             $tgt.closest('.modal').modal('show');
         }
     });
+
+    $('.readmore').each(function()
+    {
+        $(this).css({'max-height':$(this).data('max-height')});
+    });
+
+    $('.readmore-expander').on('click', function () {
+        var readme = $(this).prev('.readmore');
+        if (!this.expanded) {
+            readme.css({'overflow-y': 'visible', 'max-height': 'inherit'});
+            this.expanded = true;
+            $(this).find('i').removeClass('fa-angle-double-down').addClass('fa-angle-double-up');
+        }
+        else {
+            readme.css({'overflow-y': 'hidden', 'max-height': readme.data('max-height')});
+            this.expanded = false;
+            $(this).find('i').removeClass('fa-angle-double-up').addClass('fa-angle-double-down');
+        }
+    });
 });
 
 $(document).click(function(ev)
