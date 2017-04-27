93a8ef2b3f5b92f35aaca61730fc41a45e32b350
Nathanael d. Noblet <nathanael@noblet.ca>
2017-04-27 14:48:58 -0600

    case sensitive data value for tokenvalue

--- src/Resources/public/js/nsbootstrap.js
+++ src/Resources/public/js/nsbootstrap.js
@@ -227,8 +227,8 @@ $(document).on('nsFormUpdate shown.bs.tab shown.bs.collapse sonata.add_element a
                 $el.tokenInput('setOptions', {'url':queryUrl});
             };
 
-            if ($el.data('tokenValue')) {
-                options.tokenValue = eval($el.data('tokenValue'));
+            if ($el.data('tokenvalue')) {
+                options.tokenvalue = eval($el.data('tokenvalue'));
             }
 
             if ($el.data('resultsformatter')) {
