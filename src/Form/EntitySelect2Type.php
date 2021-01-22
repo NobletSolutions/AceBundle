c8e58aea916c95f1ef54b6b7fb952c6f84ef1686
Mark Woynarski <mark.woynarski@prolegis.ca>
2021-01-22 14:17:33 -0600

    Improvements to entity autocompleter

--- src/Form/EntitySelect2Type.php
+++ src/Form/EntitySelect2Type.php
@@ -4,16 +4,21 @@
 namespace NS\AceBundle\Form;
 
 
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Routing\RouterInterface;
 
 class EntitySelect2Type extends AbstractType
 {
-    use Select2Input;
+    use Select2Input {
+        configureOptions as _configureOptions;
+    }
 
     public function __construct(RouterInterface $router)
     {
@@ -30,15 +35,36 @@ class EntitySelect2Type extends AbstractType
         }
 
         $this->options = $options;
-        $builder->addEventListener(FormEvents::PRE_SUBMIT, [$this, 'preSubmit']);
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, [$this, 'preSetData']);
     }
 
-    public function preSubmit(FormEvent $event)
+    public function preSetData(FormEvent $event)
     {
         $form = $event->getForm();
 
-        $form->getParent()->add($form->getName(), EntitySelect2Type::class, $this->getOptions($event));
+        $data = $event->getData();
 
+        if ($data) {
+            $data = is_iterable($data) ? $data : [$data];
+
+            /** @var QueryBuilder $qb */
+            $qb = $event->getForm()->getConfig()->getOption('query_builder');
+
+            if ($qb) {
+                $alias = $qb->getRootAlias();
+                $qb->andWhere('e.id IN :es2_ids')
+                   ->setParameter('es2_ids', $data);
+            }
+        }
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $this->_configureOptions($resolver);
+
+        $resolver->setDefault('query_builder', function (EntityRepository $er) {
+            return $er->createQueryBuilder('e');
+        });
     }
 
     public function getParent()
