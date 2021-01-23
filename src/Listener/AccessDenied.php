345b3d1767ee3d17fe91ae85394083cd01563e0e
Nathanael d. Noblet <nathanael@noblet.ca>
2021-01-23 08:04:18 -0700

    Removed Twig_Extension for Twig\Extension

--- src/Listener/AccessDenied.php
+++ src/Listener/AccessDenied.php
@@ -2,38 +2,22 @@
 
 namespace NS\AceBundle\Listener;
 
-use \Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
-use \Symfony\Component\HttpFoundation\Request;
-use \Symfony\Component\Security\Core\Exception\AccessDeniedException;
-use \Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
 
-/**
- * Description of AccessDenied
- *
- * @author gnat
- */
 class AccessDenied implements AccessDeniedHandlerInterface
 {
-    /**
-     * @var \Twig_Environment
-     */
+    /** @var Environment */
     private $twig;
 
-    /**
-     * @param \Twig_Environment $twig
-     */
-    public function __construct(\Twig_Environment $twig)
+    public function __construct(Environment $twig)
     {
         $this->twig = $twig;
     }
 
-    /**
-     *
-     * @param Request $request
-     * @param AccessDeniedException $accessDeniedException
-     * @return \Symfony\Component\HttpFoundation\Response
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     */
     public function handle(Request $request, AccessDeniedException $accessDeniedException)
     {
         return new Response("Access Denied!");
