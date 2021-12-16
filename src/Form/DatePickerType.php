ad71af54b61163dce61b8e02f6a577b44f17d8db
Nathanael d. Noblet <nathanael@noblet.ca>
2021-12-16 12:59:55 -0700

    Added form options to configure the datepicker plugin

--- src/Form/DatePickerType.php
+++ src/Form/DatePickerType.php
@@ -19,10 +19,52 @@ class DatePickerType extends AbstractType
         $this->converter = ($converter) ?:new DateFormatConverter();
     }
 
+    private $dataAttributes = [
+        'end-date', //infinity
+        'start-date', //-infinity
+        'week-start', //0 (Sunday)
+        'autoclose', //false
+        'start-view', //0 'days', //(current month)
+        'title', //'',
+        'today-btn', //false
+        'today-highlight', //false
+        'toggle-active', //false
+        'z-index-offset',
+
+        //'assume-nearby-year', //false
+        //'before-show-day',
+        //'before-show-month',
+        //'before-show-year',
+        //'before-show-decade',
+        //'before-show-century',
+        //'calendar-weeks', //false
+        //'clear-btn', //false
+        //'container', //'body',
+        //'dates-disabled', //[]
+        //'days-of-week-disabled', //[]
+        //'days-of-week-highlighted', //[]
+        //'default-view-date', //today
+        //'disable-touch-keyboard', //false
+        //'enable-on-readonly', //true
+        //'force-parse', //true
+        //'immediate-updates', //false
+        //'inputs',
+        //'keep-empty-values', //false
+        //'keyboard-navigation', //true
+        //'language', //'en',
+        //'max-view-mode', //4 'centuries',
+        //'min-view-mode', //0 'days',
+        //'multidate', //false
+        //'multidate-separator', //',',
+        //'orientation', //'auto',
+        //'show-on-focus', //true
+        //'templates',
+    ];
+
     /**
      * {@inheritdoc}
      */
-    public function configureOptions(OptionsResolver $resolver)
+    public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'widget'   => 'single_text',
@@ -30,13 +72,15 @@ class DatePickerType extends AbstractType
             'format'   => $this->converter->getFormat(true),
             'html5'    => false,
         ]);
+
+        $resolver->setDefined($this->dataAttributes);
     }
 
     /**
      * {@inheritdoc}
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
-    public function buildView(FormView $view, FormInterface $form, array $options)
+    public function buildView(FormView $view, FormInterface $form, array $options): void
     {
         if (isset($view->vars['attr']['class'])) {
             $view->vars['attr']['class'] .= ' form-control date-picker';
@@ -48,12 +92,18 @@ class DatePickerType extends AbstractType
 
         $view->vars['attr']['data-date-format'] = strtolower($options['format']);
         $view->vars['attr']['placeholder']      = $options['format'];
+
+        foreach ($this->dataAttributes as $attribute) {
+            if (isset($options[$attribute])) {
+                $view->vars['attr']['data-'.$attribute] = $attribute;
+            }
+        }
     }
 
     /**
      * {@inheritdoc}
      */
-    public function getParent()
+    public function getParent(): string
     {
         return DateType::class;
     }
@@ -61,7 +111,7 @@ class DatePickerType extends AbstractType
     /**
      * @inheritDoc
      */
-    public function getBlockPrefix()
+    public function getBlockPrefix(): string
     {
         return 'acedatepicker';
     }
